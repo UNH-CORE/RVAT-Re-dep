a11509eb3ca8470febfed52c1ebedde367f2bad4
Pete Bachant <petebachant@gmail.com>
2014-12-02 18:18:27 -0500

    Progress on upload script

--- Scripts/upload.py
+++ Scripts/upload.py
@@ -36,10 +36,36 @@ def get_article_details():
     
 def get_uploaded_files():
     return get_article_details()["items"][0]["files"]
+    
+def make_local_file_list():
+    file_list = []
+    raw_dir = os.path.join("Data", "Raw")
+    sections = os.listdir(raw_dir)
+    for section in sections:
+        section_dir = os.path.join(raw_dir, section)
+        runs = os.listdir(section_dir)
+        for run in runs:
+            run_dir = os.path.join(section_dir, run)
+            files = os.listdir(run_dir)
+            for f in files:
+                fpath = os.path.join(run_dir, f)
+                file_list.append(fpath)
+    return file_list
+
+def test_upload_file():
+    fpath = make_local_file_list()[1]
+    fname = "_".join(fpath.split("\\")[-3:])
+    client = requests.session()
+    files = {"filedata" : (fpath, open(fpath, "rb"))}
+    url = "http://api.figshare.com/v1/my_data/articles/{}/files".format(article)
+    oauth = authenticate()
+    response = client.put(url, auth=oauth, files=files)
+    results = json.loads(response.content.decode())
+    print(results)
 
 if __name__ == "__main__":
     if os.getcwd()[-7:] == "Scripts":
         print("Changing working directory to experiment root directory")
         os.chdir("../")
 
-    print(get_uploaded_files())
+    test_upload_file()
