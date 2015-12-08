a6a57c60ad2f812a508248f7271821850886cd90
Pete Bachant <petebachant@gmail.com>
2015-12-08 13:23:08 -0500

    Move process.py to top level dir

---
+++ process.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Process data.
+"""
+
+from __future__ import print_function
+from pyrvatrd.processing import *
+
+if __name__ == "__main__":
+    print("Processing all data")
+    batch_process_all()
