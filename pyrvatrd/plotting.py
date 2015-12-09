1af050abbcca22597f7bab1334fb605424f768cd
Pete Bachant <petebachant@gmail.com>
2015-12-09 16:33:36 -0500

    Add explicit confidence arguments

--- pyrvatrd/plotting.py
+++ pyrvatrd/plotting.py
@@ -558,10 +558,12 @@ def plot_perf_re_dep(ax1=None, ax2=None, save=False, savedir="Figures",
         if errorbars:
             exp_unc_cp[n] = ts.calc_multi_exp_unc(df.sys_unc_cp, df.n_revs,
                                                   df.mean_cp,
-                                                  df.std_cp_per_rev, df.dof_cp)
+                                                  df.std_cp_per_rev, df.dof_cp,
+                                                  confidence=0.95)
             exp_unc_cd[n] = ts.calc_multi_exp_unc(df.sys_unc_cd, df.n_revs,
                                                   df.mean_cd,
-                                                  df.std_cd_per_rev, df.dof_cd)
+                                                  df.std_cd_per_rev, df.dof_cd,
+                                                  confidence=0.95)
     df = pd.DataFrame()
     df["Re_D"] = Re_D
     df["Re_c_ave"] = Re_c
